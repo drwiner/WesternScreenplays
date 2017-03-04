fb422e6b61ee44f5d550d4d560731a188f7fa50e
drwiner <drwiner131@gmail.com>
2017-03-04 12:17:06 -0700

    comparing category members to nouns in corpus

--- compareSeedsToOuput.py
+++ compareSeedsToOuput.py
@@ -1,23 +1,105 @@
 import spacy
 nlp = spacy.load('en')
+from numpy import dot
+from numpy.linalg import norm
+
+def cosine(v1, v2):
+	return dot(v1,v2)/(norm(v1) * norm(v2))
 
 seed_dir = 'seeds//'
 
-cluster_avgs = dict()
+def getVocab(vocab_list):
+	v_vocab = []
+	for v in vocab_list:
+		try:
+			v_vocab.append(nlp.vocab[v])
+		except:
+			continue
+	return [v for v in v_vocab if v.has_vector and v.orth_.islower()]
 
-# gather all known words, take only the lowercased versions
-category_members = set('man girl boy men women woman crowd people he she soldier rider engineer sergeant liutenant driver'.split())
+def sim(cat_words, pos_words, num_cutoff_per_word):
+	cat_dict = dict()
+	for cw in cat_words:
+		try:
+			pos_words.sort(key=lambda w: cosine(w.vector, cw.vector))
+			pos_words.reverse()
+			cndts = pos_words[:num_cutoff_per_word]
+			cat_dict[cw.orth_] = [cndt.orth_ for cndt in cndts]
+		except:
+			continue
+	running_words = set(cat_dict[cat_words[0].orth_])
+	for cw, vals in cat_dict.items():
+		running_words.intersection_update(vals)
+	return running_words
+
+
+category_members = set(
+		'man girl boy woman crowd people soldier rider engineer sergeant liutenant driver'.split())
+cat_words = getVocab(category_members)
+
+# v_expansion = set('leathers officiators concern .Dutch Redridge children Mac mob magazine warrior Everyone clerk Domino Tector Iles labor feather 11th stake Stellman em.back Bodyguard chances gunman bath'.split())
+# expansion_words = getVocab(v_expansion)
+
+all_nouns_file = open('all_nouns')
+nouns = getVocab([w.split()[0] for w in all_nouns_file])
+all_nouns_file.close()
+
+# find the most common nouns which are most similar to the category members, top 20-len(category_members)
+rw = sim(cat_words, nouns, 200)
+print(rw)
+
+
+
+for c in cat_words:
+	num = 0
+	for v in vocab_words:
+		num += cosine(c.vector, v.vector)
+	cat_dict[c.orth_] = num/len(vocab_words)
+
+print('getting all words')
 allWords = list({w for w in nlp.vocab if w.has_vector and w.orth_.islower() and w.lower_ not in category_members})
 
-# read external words to compare
-vocab_words = []
-for w in allWords:
-	pass
+print('adding cosines')
+# allWords.sort(key=lambda w: cosine(w.vector, nasa.vector))
+
+cat_dict = dict()
+for c in cat_words:
+	try:
+		allWords.sort(key=lambda w: cosine(w.vector, c.vector))
+		allWords.reverse()
+		cat_dict[c] = allWords[1:11]
+		for word in allWords[:10]:
+			print(c.orth_, word.orth_)
+		print('\n')
+	except:
+		continue
+
+# for v in vocab_words:
+# 	num = 1
+# 	for c in allWords:
+# 		try:
+# 			num += cosine(c.vector, v.vector)
+# 		except:
+# 			continue
+# 		# num += cosine(c.vector, v.vector)
+# 	# voc_dict[v.orth_] = num/len(cat_words)
+# 	voc_dict[v.orth_] = num
+
+# import operator
+# vsort = reversed(sorted(voc_dict.items(), key=operator.itemgetter(1)))
+# print(vsort)
+print('ok')
+
+
+
+
 
 def dist_avg(seed, seed_list):
 	dist_sun = 0
 	for sd in seed_list:
 		dist_sun += sd.vector
+
+
 
 def getCluster(cluster_name):
 	sd = open(seed_dir + cluster_name)
--- wordFreqs.py
+++ wordFreqs.py
@@ -17,15 +17,21 @@ if __name__ == '__main__':
 	stopwords = {wd.strip() for wd in sw}
 
 	from collections import Counter
-	tc = Counter([token.orth_ for token in doc if token.orth_ not in stopwords])
-	mcw = open('most_common words', 'w')
-	mcw.write(tc.most_common(1000))
-	mcw.close()
+	# tc = Counter([token.orth_ for token in doc if token.orth_ not in stopwords])
+	# mcw = open('most_common words', 'w')
+	# mcw.write(tc.most_common(1000))
+	# mcw.close()
 	tc_verbs = Counter([token.orth_ for token in doc if token.pos_ == 'VERB' and token.orth_ not in stopwords])
 	tc_nouns = Counter([token.orth_ for token in doc if token.pos_ == 'NOUN' and token.orth_ not in stopwords])
-	tcv = open('most_common_verbs','w')
-	tcv.write('\n'.join([str(t) for t in tc_verbs.most_common(1000)]))
+	tcv = open('all_verbs','w')
+	for t, c in tc_verbs.items():
+		tcv.write(t + '\t' + str(c) + '\n')
 	tcv.close()
-	tcn = open('most_common_nouns', 'w')
-	tcn.write('\n'.join([str(t) for t in tc_nouns.most_common(1000)]))
+	# tcv.write('\n'.join([str(t) for t in tc_verbs.most_common(1000)]))
+	# tcv.close()
+	tcn = open('all_nouns', 'w')
+	for t, c in tc_nouns.items():
+		tcn.write(t + '\t' + str(c) + '\n')
 	tcn.close()
+	# tcn.write('\n'.join([str(t) for t in tc_nouns.most_common(1000)]))
+	# tcn.close()
